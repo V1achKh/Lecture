f8d9895cc9cd3363b767d3cc08f373d7d3fc9e9f
V1achKh <VyachKheg@yandex.ru>
2026-06-07 23:32:36 +0300

    расширен класс Book в соответсвии со страницей 143

--- Library/Program.cs
+++ Library/Program.cs
@@ -4,12 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Book book1 = new Book("1984","Оруэлл", 1949, 328);
-        Book book2 = new Book("1984", "Оруэлл");
-        Book book3 = new Book();
-        Console.WriteLine(book1.GetInfo());
-        Console.WriteLine(book2.GetInfo());
-        Console.WriteLine(book3.GetInfo());
+        Book book1 =
+            new Book( "Война и мир"
+                , "Л. Толстой"
+                , 1869, 1225);
+        Console .WriteLine(book1.GetInfo());
+        Console .WriteLine(book1.GetInfo(showPages:false));
+        Console .WriteLine($"Старше 50 лет: {book1.IsOlderThan()}");
+        Console .WriteLine($"Старше 200 лет: {book1.IsOlderThan(years: 200)}");
+        Console .WriteLine(book1.GetFormattedInfo());
+        Console .WriteLine(book1.GetFormattedInfo(format:"full"));
     }
 }
 class Book
@@ -33,12 +37,41 @@ class Book
 
     public Book(): this("Без названия", "Неизвестен")
     {
-        
     }
     
     public string GetInfo()
     {
         return $"<{title}> - {author} - {year} - {pageCount}";
+    }
+
+    public string GetInfo(bool showPages)
+    {
+        if (showPages)
+        {
+            return $"<{title}> - {author} - {year} - {pageCount}";
+        }
+        else
+        {
+            return $"<{title}> - {author} - {year}";
+        }
+    }
+
+    public bool IsOlderThan(int years = 50)
+    {
+        return (DateTime.Now.Year - year) > years;
+    }
+
+    public string GetFormattedInfo(string format = "short")
+    {
+        string FShort() => $"<{title}> - {author} - {year}";
+        string FFull() => $"Название: {title} - Автор: {author} - Год: {year} - " +
+                          $"Количество страниц: {pageCount}";
+        return format switch
+        {
+            "short" => FShort(),
+            "full" => FFull(),
+            _ => GetInfo()
+        };
     }
     
 }
